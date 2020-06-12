9faa1e73d623dad458062f401974c8a42c234a64
tedim52 <tedi.m52@gmail.com>
2020-06-12 06:39:29 -0400

    Remove pw

--- backend/models/database.js
+++ backend/models/database.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require('sequelize');
 
 //setup Database
-module.exports = new Sequelize('practice', 'root', 'mikruermiasgoyasolomon', {
+module.exports = new Sequelize('practice', 'root', '', {
   host: 'localhost',
   dialect: 'mysql'
 });
