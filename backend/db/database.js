85c02b29b7ac0e6c50175282b018eb8c1d9a75ee
tedim52 <tedi.m52@gmail.com>
2020-06-14 14:39:20 -0400

    Make API keys secret

--- backend/db/database.js
+++ backend/db/database.js
@@ -2,6 +2,6 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
 //Setup db connection
-const db = new Sequelize('mysql://root:password@127.0.0.1:3306/sync');////process.env.CONNECTION_URI
+const db = new Sequelize(process.env.CONNECTION_URI);
 
 module.exports = db;
--- backend/loaders/spotify.js
+++ backend/loaders/spotify.js
@@ -3,8 +3,8 @@ var SpotifyWebApi = require('spotify-web-api-node');
 
 //Spotify API Config
 var spotifyApi = new SpotifyWebApi({
-  clientId: '06a9536a0f5643baa4b9a0da836ef0b3',//process.env.client_id
-  clientSecret: '971c2ea3a9884b8294c1e53c241d0c50'//process.env.client_secret
+  clientId: process.env.client_id,
+  clientSecret: process.env.client_secret
 });
 
 module.exports = spotifyApi;
