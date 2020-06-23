7aa891b0ce1a08c9f2f348b78971c97d7595a60a
Vaughn Campos <vcampos4545@gmail.com>
2020-06-22 21:19:20 -0400

    AHHHHH

--- backend/db/database.js
+++ backend/db/database.js
@@ -5,17 +5,10 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
-<<<<<<< HEAD
-//Setup db connection
-const db = new Sequelize(process.env.CONNECTION_URI, {
-    dialect: mysql
-});
-=======
 /**
 * Sequelize instance.
 * @module
 */
 const db = new Sequelize(process.env.CONNECTION_URI);
->>>>>>> 281c1482f61e70335ae3ed8ad17264628dd23423
 
 module.exports = db;
