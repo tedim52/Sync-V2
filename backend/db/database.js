493555824f4f98e96178ddd3db7672f63c6e7e09
Vaughn Campos <vcampos4545@gmail.com>
2020-06-22 21:10:22 -0400

    idek

--- backend/db/database.js
+++ backend/db/database.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
 //Setup db connection
-const db = new Sequelize(process.env.CONNECTION_URI);
+const db = new Sequelize(process.env.CONNECTION_URI, {
+    dialect: mysql
+});
 
 module.exports = db;
