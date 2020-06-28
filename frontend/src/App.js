04fb699778df83c62dc30545d645a270deae6aaf
Vaughn Campos <vcampos4545@gmail.com>
2020-06-28 16:51:37 -0400

    Uhh

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Nav from './Nav';
+import Home from './Home'
+import Login from './Login';
+import Users from './Users';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Sync</h1>
-      <a href="/login">Login</a>
-    </div>
-  );
+export default class App extends Component {
+
+    render() {
+        return (
+            <Router>
+            <div className="App">
+                <Nav />
+                <Switch>
+                    <Route path ="/" exact component={Home} />
+                    <Route path ="/login" component={Login} />
+                    <Route path ="/users" exact component={Users} />
+                </Switch>
+            </div>
+            </Router>
+        );
+    }
 }
-
-export default App;
---
+++ frontend/src/Users.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+async function Users() {
+    return (
+        <div>
+            <h1>Users Page</h1>
+        </div>
+    );
+}
+
+export default Users;
